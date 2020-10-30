9080a2e6f2750796da6dd44c19846db83a0c3950
r0b1337 <master@r0b.me>
2020-10-30 02:50:56 +0100

    feat(app): button component

---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">{{text}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+    .container {
+        display: inline-block;
+        padding: var(--padding-button);
+        width: fit-content;
+
+        font: var(--font-button);
+        color: var(--color-light);
+        background-color: transparent;
+
+        border: 2px solid var(--color-light);
+        border-radius: 4px;
+        cursor: pointer;
+
+        &:hover {
+            background: hsla(0, 0%, 100%, .1);
+            border-color: var(--color-light);
+        }
+    }
+</style>
