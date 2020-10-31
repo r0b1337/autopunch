6a4a72e355a2b7f73842c4e43a5e2f2e7aff2eb9
r0b1337 <master@r0b.me>
2020-10-31 10:23:54 -0700

    feat(app): emergency component

---
+++ src/components/Emergency.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="emergency">
+        <punch-parallax image="ferrari-wheel.jpg"></punch-parallax>
+        <span class="title">UNE URGENCE ?</span>
+        <span class="subtitle">Contactez nous pour toutes demandes nécessitant une intervention immédiate</span>
+        <Button class="button" text="06 61 36 05 10"></Button>
+    </div>
+</template>
+
+<script>
+import Parallax from './Parallax.vue';
+import Button from './Button.vue';
+
+export default {
+  name: 'Emergency',
+  components: {
+    'punch-parallax': Parallax,
+    Button,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+    .emergency {
+        height: auto;
+        overflow: hidden;
+        position: relative;
+        padding: var(--space-large);
+
+        .title, .subtitle {
+            margin-bottom: var(--space-large);
+        }
+
+        .button {
+            margin-bottom: var(--space-large);
+        }
+    }
+
+</style>
