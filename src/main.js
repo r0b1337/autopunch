a0a1fd10f05b6c97bf897b46e4060600a0d00204
r0b1337 <master@r0b.me>
2020-11-01 09:23:22 -0800

    feat(app): add jquery

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Parallax from './directives/Parallax.js';
 import App from './App.vue';
 
+window.$ = require('jquery');
+
 Vue.config.productionTip = false;
 Vue.use('parallax', Parallax);
 
