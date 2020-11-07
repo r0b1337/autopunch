8276bd8a9af88c665350abc6e6a9c6762dbcdf7c
ccopart <charles.copart@hotmail.fr>
2020-11-07 20:27:17 +0100

    fix jquery version

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
         ],
         script: [
             {
-                src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+                src: 'https://code.jquery.com/jquery-3.5.1.min.js',
                 type: 'text/javascript',
             },
         ],
