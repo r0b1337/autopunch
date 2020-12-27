a4a5da0da1b5e4cfbd59a1b996c430928cb6e7ad
r0b1337 <master@r0b.me>
2020-12-28 00:07:30 +0100

    feat(Comments): component with disqus

---
+++ client/components/Comments.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="comments" v-fade-in>
+        <span class="title">Ce que l'on pense de nous</span>
+        <span class="subtitle danger">Merci</span>
+        <Disqus shortname="autopunch" lang="fr_FR"></Disqus>
+    </div>
+</template>
+
+<script>
+import { Disqus } from 'vue-disqus';
+import FadeIn from '~/plugins/fade-in.client.js';
+
+export default {
+    name: 'Comments',
+    directives: {
+        FadeIn,
+    },
+    components: {
+        Disqus,
+    },
+    data () {
+        return {
+            config: {
+                language: 'fr',
+            },
+        };
+    },
+};
+</script>
+
+<style scoped lang="scss">
+    .comments {
+        padding: calc(var(--space-x-large) / 2) var(--space-large);
+        background-color: #e5ecf1;
+
+        .subtitle, .title {
+            margin-bottom: var(--space-large);
+        }
+
+        .title {
+            font: var(--font-title);
+            font-size: 36px;
+            color: var(--color-dark);
+        }
+
+        .subtitle {
+            font: var(--font-points-bold);
+        }
+    }
+</style>
