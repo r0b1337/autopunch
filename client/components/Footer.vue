2b75cb5bf1bba47fd652cd7c6daa16753db80e84
r0b1337 <master@r0b.me>
2020-12-28 22:44:37 +0100

    feat(Footer): facebook like button

--- client/components/Footer.vue
+++ client/components/Footer.vue
@@ -1,5 +1,15 @@
 <template>
     <div class="footer" v-fade-in>
+        <iframe
+            src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FMpunch63&width=180&layout=button_count&action=like&size=small&height=46&appId"
+            width="100"
+            height="46"
+            style="border:none;overflow:hidden"
+            scrolling="no"
+            frameborder="0"
+            allowfullscreen="true"
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        />
         <div class="siret">SIRET: <b>852 068 048 00010</b> © 2020</div>
         <div class="modals">
             <div class="tcu link">Conditions d'utilisation</div>
