b67113300522aeb71880cf42211f6326d5473dc6
r0b1337 <master@r0b.me>
2021-01-01 14:51:49 +0100

    feat(components): CallMe banner

---
+++ client/components/CallMe.vue
@@ -0,0 +1,39 @@
+<template>
+    <a class="call-me" href="tel:0661360510">
+        <ion-icon name="phone-portrait-outline" size="large"/>
+        <span class="number">
+            0661360510
+        </span>
+    </a>
+</template>
+
+<script>
+import FadeIn from '~/plugins/fade-in.client.js';
+
+export default {
+    name: 'CallMe',
+    directives: {
+        FadeIn,
+    },
+};
+</script>
+
+<style scoped lang="scss">
+    .call-me {
+        position: fixed;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 2;
+        bottom: 0;
+        left: 0;
+        background-color: var(--color-danger);
+        color: var(--color-light);
+        height: 52px;
+        width: 100%;
+        text-decoration: none;
+
+        ion-icon { margin-right: var(--space-small); }
+        .number { font: var(--font-button); }
+    }
+</style>
