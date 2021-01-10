261879d97522c8be0063ac798b9d3c44bdcaff10
r0b1337 <master@r0b.me>
2021-01-10 20:07:59 +0100

    fix(nuxt.config): mobile viewport + window name

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,10 +6,9 @@ export default {
 
     // Global page headers (https://go.nuxtjs.dev/config-head)
     head: {
-        title: 'autopunch',
+        title: 'Autopunch',
         meta: [
             { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
             { hid: 'description', name: 'description', content: '' },
         ],
         link: [
