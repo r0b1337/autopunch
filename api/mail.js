5c4a2fce71943dd1a00a948a6341025d5712125c
r0b1337 <master@r0b.me>
2021-01-16 21:16:42 +0100

    feat(mail): verbosity

--- api/mail.js
+++ api/mail.js
@@ -4,13 +4,17 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const nodemailer = require('nodemailer');
+const user = process.env.MAIL;
+const pass = process.env.MAIL_PASSWORD;
+console.log('user :>> ', user);
+console.log('pass :>> ', pass);
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
-        user: process.env.MAIL,
-        pass: process.env.MAIL_PASSWORD,
+        user,
+        pass,
     },
 });
 transporter.verify(console.log);
@@ -20,9 +24,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/', async (req, res) => {
+    const from = process.env.MAIL;
+    const to = process.env.DEST;
+    console.log('to :>> ', to);
+    console.log('from :>> ', from);
     const message = {
-        from: process.env.MAIL,
-        to: process.env.DEST,
+        from,
+        to,
         subject: `Nouvelle demande de ${req.body.name} / ${req.body.email} - Autopunch`,
         text: req.body.message,
     };
